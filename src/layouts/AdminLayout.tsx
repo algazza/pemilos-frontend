e967f6ba8fe08f70de74aa9ab51c8e176c873988
algazza <fadhilaswadira72564@gmail.com>
2025-09-03 06:38:12 +0700

    💄 ui: add layout for admin

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import AdminSidebar from "@/components/admin/AdminSidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
   return (
-    <div>AdminLayout</div>
-  )
-}
+    <div className='dark'>
+      <SidebarProvider>
+        <AdminSidebar />
+        <SidebarInset>
+          <main className="overflow-hidden text-foreground">
+            <Outlet />
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+    </div>
+  );
+};
 
-export default AdminLayout
+export default AdminLayout;
