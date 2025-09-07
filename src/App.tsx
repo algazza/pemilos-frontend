610bf1795138ecd6dc9a8af3fe25ec5f5c3bfba7
algazza <fadhilaswadira72564@gmail.com>
2025-09-07 09:28:58 +0700

    🔥 fix: remove admin page for vercell

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Error from "./components/Error";
-import adminRoute from "./routes/adminRoute";
 import userRoute from "./routes/userRoute";
 
 const router = createBrowserRouter([
   userRoute,
-  adminRoute,
   { path: "*", element: <Error /> },
 ]);
 
