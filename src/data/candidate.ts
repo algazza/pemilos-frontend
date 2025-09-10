5cbbd97e9bade48aec2f9c2db837a7a93e1e8bfc
algazza <fadhilaswadira72564@gmail.com>
2025-09-10 21:18:43 +0700

    ✨ feat: add candidate display data

---
+++ src/data/candidate.ts
@@ -0,0 +1,31 @@
+import Rafif from "@/assets/paslon/Rafif-1-1.svg";
+import Zahra from "@/assets/paslon/Zahra-2-1.svg";
+import Alden from "@/assets/paslon/Alden-1-q.svg";
+import Seva from "@/assets/paslon/Seva-2-2.svg";
+
+export const candidateDisplay = {
+  OSIS: [
+    {
+      id: 1,
+      name: "Rafif",
+      img: Rafif,
+    },
+    {
+      id: 2,
+      name: "Zahra",
+      img: Zahra,
+    },
+  ],
+  MPK: [
+    {
+      id: 1,
+      name: "Alden",
+      img: Alden,
+    },
+    {
+      id: 2,
+      name: "Seva",
+      img: Seva,
+    },
+  ],
+};
