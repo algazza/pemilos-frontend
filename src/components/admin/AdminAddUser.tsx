832b790c3c4d8225154410dfef025fd30affc6b3
algazza <fadhilaswadira72564@gmail.com>
2025-09-20 22:42:37 +0700

    ✨ feat: hit api for add user

--- src/components/admin/AdminAddUser.tsx
+++ src/components/admin/AdminAddUser.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Dialog,
   DialogClose,
@@ -10,73 +10,114 @@ import {
 } from "../ui/dialog";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
-import type { UserType } from "@/schemas/user.schema";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from "../ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
 import { Button } from "../ui/button";
+import { classOptions } from "@/lib/class";
+import axios from "axios";
+import { apiUrl } from "@/lib/api";
+import { toast } from "sonner";
 
 const AdminAddUser = ({
   children,
-  user,
   isNewUser,
+  refetch,
 }: {
   children: React.ReactNode;
-  user?: UserType;
-  isNewUser: boolean
+  isNewUser: boolean;
+  refetch: () => void;
 }) => {
+  const [kelas, setKelas] = useState("");
+  const [open, setOpen] = useState(true);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    const name = formData.get("name") as string;
+    const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
+    const kelas = formData.get("kelas") as string;
+
+    const data = {
+      name,
+      username,
+      password,
+      kelas,
+      role: "voter",
+    };
+
+    console.log(data)
+
+    try {
+      await axios.post(`${apiUrl}/admin/user`, data);
+      refetch();
+      toast("User Berhasil dibuat")
+      setOpen(false)
+    } catch(error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Dialog>
-      <form>
-        <DialogTrigger className="cursor-pointer">{children}</DialogTrigger>
-        <DialogContent className="sm:max-w-[425px] dark text-foreground">
+    <Dialog modal={open} onOpenChange={setOpen}>
+      <DialogTrigger className="cursor-pointer">{children}</DialogTrigger>
+      <DialogContent className="sm:max-w-[425px] dark text-foreground">
+        <form onSubmit={handleSubmit}>
           <DialogHeader>
-            <DialogTitle>{isNewUser ? 'Tambah user' :'Edit user'}</DialogTitle>
+            <DialogTitle>Tambah User</DialogTitle>
           </DialogHeader>
-          <div className="grid gap-4">
+          <div className="grid gap-4 py-4">
             <div className="grid gap-3">
               <Label htmlFor="">Nama</Label>
-              <Input name="name" defaultValue={user?.name} />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="">Kelas</Label>
-              <Input name="grade" defaultValue={user?.class} />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="">UUID</Label>
-              <Input name="uuid" defaultValue={user?._id} />
+              <Input name="name" />
             </div>
             <div className="grid gap-3">
               <Label htmlFor="">Username</Label>
               <Input
                 id="username-1"
                 name="username"
-                defaultValue={user?.username}
               />
             </div>
             <div className="grid gap-3">
-              <Label htmlFor="">Role</Label>
-              <Select defaultValue={user?.role}>
+              <Label htmlFor="">Password</Label>
+              <Input name="password" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="">Kelas</Label>
+              <Select onValueChange={setKelas}>
                 <SelectTrigger className="w-full">
                   <SelectValue placeholder="Select role" />
                 </SelectTrigger>
                 <SelectContent className="dark text-foreground">
                   <SelectGroup>
-                    <SelectItem value="Murid">Murid</SelectItem>
-                    <SelectItem value="Guru">Guru</SelectItem>
-                    <SelectItem value="Staff">Staff</SelectItem>
-                    <SelectItem value="Admin">Admin</SelectItem>
+                    {classOptions.map((clas) => (
+                      <SelectItem key={clas} value={clas}>
+                        {clas}
+                      </SelectItem>
+                    ))}
                   </SelectGroup>
                 </SelectContent>
               </Select>
+              <input type="hidden" name="kelas" value={kelas} />
             </div>
           </div>
           <DialogFooter>
             <DialogClose asChild>
               <Button variant="outline">Cancel</Button>
             </DialogClose>
-            <Button type="submit">{isNewUser ? 'Add User' :'Save changes'}</Button>
+            <Button type="submit">
+              {isNewUser ? "Add User" : "Save changes"}
+            </Button>
           </DialogFooter>
-        </DialogContent>
-      </form>
+        </form>
+      </DialogContent>
     </Dialog>
   );
 };
--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,5 +1,6 @@
 import AdminSidebar from "@/components/admin/AdminSidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Toaster } from "@/components/ui/sonner";
 import { Outlet } from "react-router-dom";
 
 const AdminLayout = () => {
@@ -11,6 +12,7 @@ const AdminLayout = () => {
           <main className="overflow-hidden text-foreground p-8">
             <Outlet />
           </main>
+          <Toaster />
         </SidebarInset>
       </SidebarProvider>
     </div>
