b99e5a8eb64ac5899a4c4b169f83186594cb8a7d
algazza <fadhilaswadira72564@gmail.com>
2025-09-20 20:50:03 +0700

    ✨ feat: hit api for search, refresh, and delete

--- src/components/admin/AdminVote.tsx
+++ src/components/admin/AdminVote.tsx
@@ -1,7 +1,21 @@
 import type { UserType } from "@/schemas/user.schema";
 import { type ColumnDef } from "@tanstack/react-table";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "../ui/alert-dialog";
+import { RefreshCcw } from "lucide-react";
+import axios from "axios";
+import { apiUrl } from "@/lib/api";
 
-export const columns: ColumnDef<UserType>[] = [
+
+export const columns = (refetch: () => void): ColumnDef<UserType>[] => [
   {
     accessorKey: "name",
     header: "Nama",
@@ -22,15 +36,15 @@ export const columns: ColumnDef<UserType>[] = [
       const role = row.original.role;
       return (
         <div
-          className={`rounded-lg w-fit px-2 text-sm font-semibold ${
-            role === "Murid"
-              ? "bg-blue-500"
-              : role === "Staff"
-              ? "bg-orange-500"
-              : role === 'Guru' 
-              ? "bg-green-500"
-              : 'bg-red-500' 
-          }`}
+        className={`rounded-lg w-fit px-2 text-sm font-semibold ${
+          role === "Murid"
+          ? "bg-blue-500"
+          : role === "Staff"
+          ? "bg-orange-500"
+          : role === "Guru"
+          ? "bg-green-500"
+          : "bg-red-500"
+        }`}
         >
           {role}
         </div>
@@ -38,18 +52,63 @@ export const columns: ColumnDef<UserType>[] = [
     },
   },
   {
-    accessorKey: 'vote',
-    header: 'Vote',
-    cell: ({row}) => {
-      const vote = row.original.isVoted
+    accessorKey: "vote",
+    header: "Vote",
+    cell: ({ row }) => {
+      const vote = row.original.isVoted;
+      
+      return (
+        <div
+        className={`rounded-lg w-fit px-2 text-sm font-semibold ${
+          vote ? "bg-white text-black" : "border-2 border-white"
+        }`}
+        >
+          {vote ? "Sudah" : "Belum"}
+        </div>
+      );
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const user = row.original;
+    
+      const ResetData = async (username: string) => {
+        try {
+          await axios.put(`${apiUrl}/admin/reset`, { username });
+          refetch();
+        } catch (error) {
+          console.error("Failed to reset user:", error);
+          throw error;
+        }
+      };
 
       return (
-        <div className={`rounded-lg w-fit px-2 text-sm font-semibold ${
-          vote ? 'bg-white text-black' : 'border-2 border-white'
-        }`}>
-          {vote ? 'Sudah' : 'Belum'}
+        <div className="flex gap-8">
+          <AlertDialog>
+            <AlertDialogTrigger className="cursor-pointer">
+              <RefreshCcw className="size-4" />
+            </AlertDialogTrigger>
+            <AlertDialogContent className="dark text-foreground">
+              <AlertDialogHeader>
+                <AlertDialogTitle>
+                  Apakah ada yakin ingin mereset user ini?
+                </AlertDialogTitle>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                <AlertDialogAction
+                  className="bg-blue-500 text-white"
+                  onClick={() => ResetData(user.username)}
+                  disabled={!user.isVoted}
+                >
+                  Reset
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
         </div>
-      )
-    }
-  }
+      );
+    },
+  },
 ];
